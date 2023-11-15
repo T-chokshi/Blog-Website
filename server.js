508a295908936daae29936b1e814bdc45b7bf741
Tirth chokshi <122982506+T-chokshi@users.noreply.github.com>
2023-11-15 19:21:58 +0530

    completed the project

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+mongoose.connect('mongodb://localhost/blog')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
+
+app.get('/', async(req, res) => {
+   const articles = await Article.find().sort({ createdAt: 'desc'})
+    res.render('articles/index', { article: articles })
+})
+
 
 app.use('/articles', articleRouter)
 
